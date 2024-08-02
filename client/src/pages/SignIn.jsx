d1f6c41871699a968c3c2f3ad5082759115cefae
Ashish Kumar Singh <ashishksingh00110@gmail.com>
2024-08-03 02:23:34 +0530

    some changes

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -43,7 +43,7 @@ const SignIn = () => {
 
     return (
 
-        <div className="min-h-fit mt-20">
+        <div className="min-h-screen mt-20">
 
             <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5 '>
                 {/* left side */}
