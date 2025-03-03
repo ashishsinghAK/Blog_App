bd7fafed9e630e91fad0b83955fe3a2a0e73ddea
ashishsinghAK <130988873+ashishsinghAK@users.noreply.github.com>
2025-03-03 15:46:22 +0530

    Update PostPage.jsx URL

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -19,7 +19,7 @@ const PostPage = () => {
         const fetchPost = async () => {
             try {
                 setLoading(true);
-                const res = await fetch(`${process.env.REACT_APP_BASE_URLL}/api/post/getpost?slug=${postSlug}`);
+                const res = await fetch(`${process.env.REACT_APP_BASE_URL}/api/post/getpost?slug=${postSlug}`);
                 const data = await res.json();
                 if (!res.ok) {
                     setError(true);
